e5a507f205353835bc731554441a17fa31d50f46
Justin Johnson <elofjohnson@gmail.com>
2013-09-05 15:20:14 -0700

    added skills to user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,8 @@ class UsersController < ApplicationController
     	@user = User.find params[:id]
 		end
 		
+		@skillset = @user.skillsets.build
+
 		@api_activity = []
 
 		unless @user.hireable.nil?
--- db/migrate/20130905184324_change_skillset_skillable_id_to_object_id.rb
+++
@@ -1,6 +0,0 @@
-class ChangeSkillsetSkillableIdToObjectId < ActiveRecord::Migration
-	def change
-		rename_column(:skillsets, :skillable_id, :object_id)
-		rename_column(:skillsets, :skillable_type, :object_type)
-	end
-end
