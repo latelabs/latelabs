f04d0f5537ec14cb9776e9941d7a9512f094c39a
Justin Johnson <elofjohnson@gmail.com>
2013-09-05 18:35:21 -0700

    finished skills for users and for projects, implemented basic search

--- app/controllers/skillsets_controller.rb
+++ app/controllers/skillsets_controller.rb
@@ -1,4 +1,12 @@
 class SkillsetsController < ApplicationController
+	
+	before_filter :authenticate_user!
+
+	def index
+		@allskills = Skillset.search(params[:search])
+		@user_skills = @allskills.select {|skillset| skillset.skillable_type == 'User'}
+		@project_skills = @allskills.select {|skillset| skillset.skillable_type == 'Project'}
+	end
 
 	def create
 		@skill = Skill.where(:skill => params[:skill][:skill]).first 
--- app/models/skillset.rb
+++ app/models/skillset.rb
@@ -16,5 +16,22 @@ attr_accessible :skillable_id, :skillable_type
 belongs_to :skill 
 
 belongs_to :skillable, :polymorphic => true
+
+def self.search(search)
+	if search.nil? or search.length == 0
+		find(:all)
+	else
+		# find all skills by name
+		# use skill id to retrieve skillset
+		# skillset
+		# find all skills by na
+		allskills = Skill.where "skill LIKE '%#{search}%'"
+		allskillsets = []
+		allskills.each do |skill|
+			allskillsets << skill.skillsets
+		end
+		allskillsets.flatten
+	end
+end
   
 end
