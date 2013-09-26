3febce06b4902457ac3ff03ecfcaffd34c368e5d
nathantross <nathantross@gmail.com>
2013-09-26 13:30:14 -0700

    fixed dev environment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20130919001512) do
 
   create_table "applications", :force => true do |t|
